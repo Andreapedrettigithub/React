ad9e5c2d2bed0525362af1b6aa686a46d7a9dceb
Andreapedrettigithub <andrebenjanapo1@gmail.com>
2023-05-22 16:29:37 -0600

    navbar y boostrap con react

--- mi-primera-app/src/App.js
+++ mi-primera-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Button, Navbar, Nav, Container } from 'react-bootstrap';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            <Button variant="outline-success">Sign In</Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
 
 export default App;
+
+
